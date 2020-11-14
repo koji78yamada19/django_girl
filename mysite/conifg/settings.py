58be91a2e4f4605ee1dc60dd8e799918f7ca8931
vagrant <vagrant@vagrant.vm>
2020-11-14 04:11:21 +0000

    sqliteに変更

--- mysite/conifg/settings.py
+++ mysite/conifg/settings.py
@@ -79,15 +79,10 @@ WSGI_APPLICATION = 'conifg.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
